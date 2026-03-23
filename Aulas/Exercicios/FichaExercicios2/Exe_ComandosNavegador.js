77483599b28d0f9620042476d6c363420e0d79a1
Eduardo Kim <devkim.eduardo@gmail.com>
2026-03-23 20:02:11 -0300

    Exercitando sobre a Aula 06

---
+++ Aulas/Exercicios/FichaExercicios2/Exe_ComandosNavegador.js
@@ -0,0 +1,7 @@
+const VALOR1 = prompt('Digite um valor :');
+const VALOR2 = prompt('Digite outro valor : ');
+
+const RESULTADO = parseInt(VALOR1) + parseInt(VALOR2);
+
+
+alert(`O resultado da operação é ${RESULTADO}`);
